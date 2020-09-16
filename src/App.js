ad1d72c3b64ed765bee97edecb8e57d0c6ce8114
Menas Erian <meena5.erian@gmail.com>
2020-09-16 13:03:35 +0400

    pre-cache image files before rendering the page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,55 @@ import React from 'react';
 import './App.css';
 import { PageContextProvider } from "./component/PageContext";
 import Wizard from "./component/Wizard";
+import { images } from "./constants";
 
-function App() {
-  return (
-    <PageContextProvider>
-      <div className="App">
-        <header className="App-header">
+// Converting images object into array
+var imagesArray = [];
+Object.keys(images).forEach((key)=>{
+  imagesArray.push(images[key]);
+});
+
+function casheImage(resArr){
+  return resArr.map(src => {
+    return new Promise(function (resolve, reject){
+      const img = new Image();
+      img.src = src;
+      img.onload = resolve;
+      img.onerror = reject;
+    });
+  });
+}
+
+class App extends React.Component {
+
+  state = {resourcesLoaded: false}
+
+  componentDidMount(){
+    Promise.all(casheImage(imagesArray)).then(()=>{
+      this.setState({resourcesLoaded: true});
+    });
+  }
+  
+  render() {
+    if(!this.state.resourcesLoaded){
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Loading App Resources</h1>
+          </header>
+        </div>
+      );
+    }
+    return (
+      <PageContextProvider>
+        <div className="App">
+          <header className="App-header">
             <Wizard />
-        </header>
-      </div>
-    </PageContextProvider>
-  );
+          </header>
+        </div>
+      </PageContextProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/constants/images.ts
@@ -0,0 +1,7 @@
+const images = {
+    BACK_GROUND : "/assets/background.png",
+    LOGO_SYMBOL_SMALL : "/assets/logo-white.png",
+    LOGO_SMALL : "/assets/everlast-white.png"
+};
+
+export default images;
