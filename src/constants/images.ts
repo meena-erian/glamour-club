c691d16ec97b3a23027707fbd7324fa24fce8d76
Menas Erian <meena5.erian@gmail.com>
2020-09-16 16:55:38 +0400

    Define image files in the code for pre-load and code-friendly access

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,22 @@
 const images = {
-    BACK_GROUND : "/assets/background.png",
+    BACKGROUND : "/assets/background.png",
     LOGO_SYMBOL_SMALL : "/assets/logo-white.png",
-    LOGO_SMALL : "/assets/everlast-white.png"
+    LOGO_SMALL : "/assets/everlast-white.png",
+    LOGO_NAV : "/assets/everlast.png",
+    FACE_MASK : "/assets/face-mask.png",
+    FRONT_CLOUD : "/assets/front-cloud.png",
+    WOMENS_DAY : "/assets/womens-day-hr.png",
+    TWO_GIRLS : "/assets/2girls.png.png",
+    BACK_CLOUD : "/assets/2girls.png",
+    GIRL_00 : "/assets/girl-00.png",
+    GIRL_1 : "/assets/girl-1.png",
+    GIRL_2 : "/assets/girl-2.png",
+    GIRL_3 : "/assets/girl-3.png",
+    GIRL_4 : "/assets/girl-4.png",
+    GIRL_5 : "/assets/girl-5.png",
+    GIRL_6 : "/assets/girl-6.png",
+    GIRL_7 : "/assets/girl-7.png",
+    GIRL_8 : "/assets/girl-8.png",
 };
 
 export default images;
