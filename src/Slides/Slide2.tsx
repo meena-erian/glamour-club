626dde39d3a9683bdfbf60a84f7e3cd685ede61c
Menas Erian <meena5.erian@gmail.com>
2020-09-17 17:08:00 +0400

    Prepare next slides

--- src/Slides/Slide2.tsx
+++ src/Slides/Slide2.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import Cloud from "../component/Cloud";
+import Float from "../component/Float";
+import AnimatedBorder from "../component/AnimatedBorder";
+import AnimatedLogo from "../component/AnimatedLogo";
 
-export default function (props: { isMobile:boolean, index: number, next: any, setArabic: any, back: any }) {
-    return (
-        <div className="animation-canvas">
-            <h1>{props.index}</h1>
-            <img src={"/assets/back-cloud.png"} alt={"slide-"+props.index} />
-            <button onClick={props.back}>Back</button>
-            <button onClick={props.next}>Next</button>
-        </div>
-    );
+export default class Slide extends React.PureComponent<{ isMobile: boolean, index: number, next: any, setArabic: any, back: any }> {
+    public state = { stage: 0 };
+    render() {
+        const { index, back, next, isMobile } = this.props;
+        const stage = this.state.stage
+        return (
+            <div className="animation-canvas">
+                <Float x={0} y={-2} style={{ zIndex: 99 }}>
+                    <AnimatedBorder stage={4} isMobile={isMobile} nextSlide={next} />
+                </Float>
+                <Float x={0} y={0} style={{ zIndex: 100 }}>
+                    <AnimatedLogo isMobile={isMobile} stage={4} />
+                </Float>
+                <Float x={0} y={200}>
+                    <Cloud stage={0} />
+                </Float>
+            </div>
+        );
+    }
 }
