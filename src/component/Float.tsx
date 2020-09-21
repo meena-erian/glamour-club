76eab9ea3caad5b488a554c90007fbf6a6e68091
Menas Erian <meena5.erian@gmail.com>
2020-09-21 08:50:29 +0400

    Allow Float object to take dimention properties in different formats

--- src/component/Float.tsx
+++ src/component/Float.tsx
@@ -1,8 +1,8 @@
 import React, { CSSProperties } from "react";
 
 interface FloatProps{
-    x:number; // X Displacement from the onigin in pixels
-    y:number; // Y Displacement from the origin in pixels
+    x:number|string; // X Displacement from the onigin in pixels
+    y:number|string; // Y Displacement from the origin in pixels
     transitionDelay?: string;
     style?: CSSProperties;
 }
@@ -10,11 +10,13 @@ interface FloatProps{
 export default class Float extends React.Component<FloatProps>{
     render(){
         const transitionDelay = this.props.transitionDelay || "1s";
+        const x = (typeof this.props.x === 'string')? this.props.x : this.props.x + 'px';
+        const y = (typeof this.props.y === 'string')? this.props.y : this.props.y + 'px';
         return (
             <div style={{
                 position: "absolute",
-                left: `calc( 50% + ${this.props.x}px )`,
-                top: `calc( 50% + ${this.props.y}px )`,
+                left: `calc( 50% + ${x} )`,
+                top: `calc( 50% + ${y} )`,
                 transform: "translate(-50%, -50%)",
                 transition: `all ${transitionDelay} ease`,
                 ...this.props.style
