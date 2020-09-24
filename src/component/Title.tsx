58a831f4060420f815884ff94a7bc3d4d1ed1183
Menas Erian <meena5.erian@gmail.com>
2020-09-24 20:06:38 +0400

    fix typo

--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -5,7 +5,7 @@ export default function Title(props: { arabic: boolean, stage: number, relative?
     return <div style={{ fontSize: 20, transition: "all 0.5s ease", transform: stage > 3 ? "scale(0.7)  translateY(-10px)" : "translateY(-10px)", display: relative ? "inline-block" : "block" }}>
         <h2 style={{ margin: 0 }}>{arabic ? 'نادى الإشراق' : 'Glamour Club'}</h2>
         <p style={{ margin: 0, transform: "translateY(-11px)" }}>
-            <small>{arabic ? 'العضوية السانوية' : 'Annual Membership'}</small>
+            <small>{arabic ? 'العضوية السنوية' : 'Annual Membership'}</small>
         </p>
     </div>;
 }
